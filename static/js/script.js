ae4ba4acecfd13e39644ed101cee18b986c16c4b
Voggastur <v3x0r_x014@hotmail.com>
2020-07-17 20:21:03 +0000

    added data characterCounter for textareas, tweaked slider options

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".modal").modal();
-  $('select').formSelect();
-  $('.card').css('margin-bottom', '40px');
+  $("select#adventure").formSelect();
+  $("#possessions").characterCounter();
   const slider = document.querySelector(".slider");
   M.Slider.init(slider, {
     indicators: false,
