6c5f72c7cbd52548cdaac6f8b08d02685172c092
Voggastur <v3x0r_x014@hotmail.com>
2020-07-21 01:15:23 +0000

    added materialize collapsible with options to improve text readability

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,25 @@
+// Materialize functions
 $(document).ready(function () {
-  $(".modal").modal();
   $("select#adventure").formSelect();
-  $("#possessions").characterCounter();
-  const slider = document.querySelector(".slider");
+  $("textarea").characterCounter();
+  $(".modal").modal();
+  // Materialize collapsible with options
+  $(".collapsible").collapsible({
+      onOpenStart() {
+          $(this).find(".target").removeClass("visibility"); // This makes my less important paragraphs visible on clicking card
+          $(this).find("p", "h6", "i").addClass("transform_text"); // This class improves the readability of the text
+      },
+      onCloseStart() {
+          $(this).find(".target").addClass("visibility"); // This makes my chosen paragraphs invisible again
+          $(this).find("p", "h6", "i").removeClass("transform_text"); // Revert to normal text
+      }
+  });
+  
+  // Materialize slider function
+  let slider = document.querySelector(".slider");
   M.Slider.init(slider, {
     indicators: false,
-    height: 400,
+    height: 500,
     transition: 500,
     interval: 6000,
   });
