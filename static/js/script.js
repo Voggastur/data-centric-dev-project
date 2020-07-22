9fb82808c049b0df7ee50b80931c0d92cae0a836
Voggastur <v3x0r_x014@hotmail.com>
2020-07-22 17:13:06 +0000

    removed redundant options in collapsible, hide .target spans with jquery

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,16 @@
 // Materialize functions
 $(document).ready(function () {
-  $("select#adventure").formSelect();
+  $("select").formSelect();
   $("textarea").characterCounter();
   $(".modal").modal();
-  // Materialize collapsible with options
-  $(".collapsible").collapsible({
-      onOpenStart() {
-          $(this).find(".target").removeClass("visibility"); // This makes my less important paragraphs visible on clicking card
-          $(this).find("p", "h6", "i").addClass("transform_text"); // This class improves the readability of the text
-      },
-      onCloseStart() {
-          $(this).find(".target").addClass("visibility"); // This makes my chosen paragraphs invisible again
-          $(this).find("p", "h6", "i").removeClass("transform_text"); // Revert to normal text
-      }
-  });
-  
+  $(".collapsible").collapsible();
+  // Hide header spans
+  $("h4.card-text .target").hide();
   // Materialize slider function
   let slider = document.querySelector(".slider");
   M.Slider.init(slider, {
     indicators: false,
-    height: 500,
+    height: 400,
     transition: 500,
     interval: 6000,
   });
